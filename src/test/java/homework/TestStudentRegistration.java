af7ca2d765b9eb1fd35d6b7cff4c7753eb93a723
Khodzhoyan Vardan <xva333@gmail.com>
2023-03-29 10:06:08 +0300

    Homework 2

---
+++ src/test/java/homework/TestStudentRegistration.java
@@ -0,0 +1,51 @@
+package homework;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class TestStudentRegistration extends TestBase{
+
+    @Test
+    void successfulStudentRegistration() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Lyffi");
+        $("#lastName").setValue("Mugivara");
+        $("#userEmail").setValue("Lyffi@gmail.com");
+        $(".custom-control-label").click();
+        $("#userNumber").setValue("9999999999");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").$(byText("July")).click();
+        $(".react-datepicker__year-select").$(byText("1987")).click();
+        $(".react-datepicker__day--0" + "12").click();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/cat.png"));
+        $("#currentAddress").setValue("Grande Line");
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
+        $("#submit").click();
+
+        $(".table-responsive").shouldHave(
+                text("Lyffi"),
+                text("Mugivara"),
+                text("Lyffi@gmail.com"),
+                text("Male"),
+                text("9999999999"),
+                text("12 July,1987"),
+                text("Computer Science"),
+                text("Sports"),
+                text("cat.png"),
+                text("Grande Line"),
+                text("Haryana"),
+                text("Karnal")
+        );
+
+    }
+}
